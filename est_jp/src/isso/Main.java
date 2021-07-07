d986309832fbb673cbe2b84ae2d33f88cf361560
joaope-code <86989613+joaope-code@users.noreply.github.com>
2021-07-07 02:04:30 -0300

    Add files via upload

---
+++ est_jp/src/isso/Main.java
@@ -0,0 +1,40 @@
+package isso;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+//       boolean verdade = true;
+//       boolean falso = false;
+
+        Scanner scan = new Scanner (System.in);//importante pakas
+//
+//        System.out.println("Digite seu nome completo:");
+//        String nomeCompleto = scan.nextLine();
+//        System.out.println("Seu nome é " + nomeCompleto);
+//
+//        System.out.println("Digite seu primeiro nome");
+//        String primeiroNome = scan.next();
+//        System.out.println("Seu primeiro nome é "+ primeiroNome);
+
+        System.out.println("digite a sua idade");
+        int id1 = scan.nextInt();
+        System.out.println("Sua idade é " + id1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
